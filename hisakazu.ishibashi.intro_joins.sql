37a1580f1ce163b992e792ceb84a2d784c926e21
hisa <kazutron@gmail.com>
2013-03-11 00:16:06 -0300

    Archivo de ejemplo de entrega del trabajo-1. Este commit hace referencia al issue #1

---
+++ hisakazu.ishibashi.intro_joins.sql
@@ -0,0 +1,13 @@
+/**
+ Trabajo #1 - Base de Datos 2013 - UNI
+ 
+ Respuesta a los ejercicios propuestos
+ 
+ Alumno: Hisakazu Ishibashi - kazutron@gmail.com 
+ Obs: Este es un archivo de ejemplo
+*/
+
+use intro_joins;
+
+-- 1) Lista de todos los miembros
+Select * from members;
