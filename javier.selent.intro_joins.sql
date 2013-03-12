efae6e8364bd6c3364ad26bf4668148bd16f9a53
Javier Rios Selent <69javiselent@gmail.com>
2013-03-12 13:12:03 -0400

    he creado el archivo de entrega del trabajo #1

---
+++ javier.selent.intro_joins.sql
@@ -0,0 +1,13 @@
+/**
+ Trabajo #1 - Base de Datos 2013 - UNI
+ 
+ Respuesta a los ejercicios propuestos
+ 
+ Alumno: Javier Selent - 69javiselent@gmail.com 
+
+*/
+
+use intro_joins;
+
+-- 1) Lista de todos los miembros
+Select * from members;
