52b7d8f7866f7cdcb721bec1d8881006a977c297
ferkoni <ferkoni@gmail.com>
2013-03-14 19:14:46 +0100

    Update fernando.konishi.intro_joins.sql

--- intro-sql-joins/fernando.konishi.intro_joins.sql
+++ intro-sql-joins/fernando.konishi.intro_joins.sql
@@ -5,3 +5,9 @@ Select * from members
 Select * from books
 3) Ver que libro tiene actualmente el miebro dave
 Select 
+4) Ver que miembros no tienen prestado ningun libro
+select id, name from members m left join loans l on m.id=member_id where member_id is null;
+5) Ver todos los libros en stock
+select title from books left join loans on id = loans.book_id where loans.member_id is null;
+6) Lista de todos los titulos prestados actualmente con el nombre de los
+socios que lo prestaron
